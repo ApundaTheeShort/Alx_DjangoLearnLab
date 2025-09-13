6d160ecada3b4c4f6186c6d33716ec5ac5dcc75d
Apunda <apundawyclife333@gmail.com>
2025-09-13 13:52:51 +0300

    added author searching

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,4 +1,4 @@
-from .models import Book, Library, Author
+from .models import Book, Library, Author, Librarian
 
 author = Author.objects.get(name=author_name)
 books = Book.objects.filter(author=author)
@@ -6,5 +6,5 @@ books = Book.objects.filter(author=author)
 library = Library.objects.get(name=library_name)
 library_books = library.books.all()
 
-library = Library.objects.get(name=library_name)
-librarian = library.librarian
+get_library = Library.objects.get(name=library_name)
+librarian = Librarian.objects.get(library=get_library)
