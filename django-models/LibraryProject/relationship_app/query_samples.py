6bfbd33e5ba9db3c19a4a2e9459728cbdb00a27d
Apunda <apundawyclife333@gmail.com>
2025-09-13 13:46:43 +0300

    updated quering by author

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,6 +1,6 @@
 from .models import Book, Library
 
-books = Book.objects.filter(author=author_name)
+books = Book.objects.filter(author=author)
 
 library = Library.objects.get(name=library_name)
 library_books = library.books.all()
