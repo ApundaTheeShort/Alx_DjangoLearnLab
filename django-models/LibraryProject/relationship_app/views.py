01006d013ad6b4c5751071b7016c2cbd88f7b0a9
Apunda <apundawyclife333@gmail.com>
2025-09-22 22:55:31 +0300

    changed admin view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -47,7 +47,7 @@ def register(request):
 
 
 def is_admin(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == "Admin"
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
 
 def is_librarian(user):
     return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
@@ -57,7 +57,7 @@ def is_member(user):
 
 
 @user_passes_test(is_admin)
-def admin_view(request):
+def Admin(request):
     return render(request, 'relationship_app/admin_view.html')
 
 @user_passes_test(is_librarian)
