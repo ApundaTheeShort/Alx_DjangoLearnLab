55f4c30128f31843b02289c5f70174a51599a745
Apunda <apundawyclife333@gmail.com>
2025-09-22 22:52:33 +0300

    added models, urls & views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,16 @@
-from django.urls import path
-from .views import list_books, LibraryDetailView
-from . import views
 from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path, include
+# from .views import list_books, LibraryDetailView
+from . import views
 
 urlpatterns = [
+    # path("", views.home, name="home"),
     path("list_books/", list_books, name="list_books"),
     path("library_details/", LibraryDetailView.as_view(), name="library_details"),
     path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
     path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
     path("register/", views.register, name="register"),
+    path("admin-dashboard/", views.admin_view, name="admin_view"),
+    path("librarian-dashboard/", views.librarian_view, name="librarian_view"),
+    path("member-dashboard/", views.member_view, name="member_view"),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,10 +2,11 @@
 from django.contrib.auth import login
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.forms import UserCreationForm
 from .models import Library
 from .models import Book
 # Create your views here.
-from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 
@@ -29,6 +30,10 @@ class LibraryDetailView(DetailView):
 #     success_url = reverse_lazy('login')
 #     template_name = 'relationship_app/register.html'
 
+# @login_required
+# def home(request):
+#     return render(request, "home.html", {})
+
 
 def register(request):
     if request.method == "POST":
@@ -39,3 +44,26 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, "relationship_app/register.html", {"form": form})
+
+
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == "Admin"
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
