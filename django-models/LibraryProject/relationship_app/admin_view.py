11b774f42cdef048cf424ef0fe124b5477add045
Apunda <apundawyclife333@gmail.com>
2025-09-22 23:12:06 +0300

    separated admin view

--- django-models/LibraryProject/relationship_app/admin_view.py
+++ django-models/LibraryProject/relationship_app/admin_view.py
@@ -2,6 +2,9 @@ from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render
 
 
-@user_passes_test(lambda u: u.userprofile.role == 'Admin')
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+@user_passes_test(is_admin)
 def admin_view(request):
     return render(request, 'relationship_app/admin_view.html')
