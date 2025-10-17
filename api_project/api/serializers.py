16fdb5c1d8a88bd4dec1a85489759b41c4ccecb1
Apunda <apundawyclife333@gmail.com>
2025-10-17 12:54:36 +0300

    feat: add BookSerializer for the Book model

---
+++ api_project/api/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import Book
+
+
+class BookSerializer(serializers.ModelSerializer):
+
+    class Meta:
+
+        model = Book
+        fields = "__all__"  # includes all fields defined in the Book model
