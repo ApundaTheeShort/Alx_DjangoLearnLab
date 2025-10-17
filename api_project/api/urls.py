68b0a4eb5c96414a5a877154eede54ad0a1a9810
Apunda <apundawyclife333@gmail.com>
2025-10-17 13:38:16 +0300

    feat: implement BookViewSet and update URL routing for Book API

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path, include
+from .views import BookViewSet
 from .views import BookList
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('books_all', BookViewSet, basename='books_all')
 
 urlpatterns = [
-    path('books/', BookList.as_view(), name='book-list')
+    # Route for the BookList view (ListAPIView)
+    path('books/', BookList.as_view(), name='book-list'),
+
+    # Include the router URLs for BookViewSet (all CRUD operations)s
+    # This includes all routes registered with the router
+    path('', include(router.urls)),
 ]
