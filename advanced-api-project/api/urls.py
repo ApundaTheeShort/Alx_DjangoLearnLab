ea9eacc9fa14aa6d4b96d6faa48dc233666fad87
Apunda <apundawyclife333@gmail.com>
2025-10-19 17:49:42 +0300

    fix: correct URL patterns for book update and delete views

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('books/create/', views.CreateView.as_view(), name='books'),
     path('books/<int:pk>/', views.DetailView.as_view(), name='book-detail'),
-    path('books/<int:pk>/update/', views.UpdateView.as_view(), name='book-update'),
-    path('books/<int:pk>/delete/', views.DeleteView.as_view(), name='book-delete'),
+    path('books/update/<int:pk>/', views.UpdateView.as_view(), name='book-update'),
+    path('books/delete/<int:pk>/', views.DeleteView.as_view(), name='book-delete'),
     path('books/', views.ListView.as_view(), name='book-list'),
     path('authors/', views.CreateAuthorView.as_view(), name='authors'),
 ]
