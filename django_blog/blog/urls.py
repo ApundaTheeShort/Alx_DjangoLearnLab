ff811f4599c0a23d22ab33d2b8469c61b8ef40eb
Apunda <apundawyclife333@gmail.com>
2025-10-22 10:42:56 +0300

    fix: correct comment URL paths for consistency

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -12,8 +12,8 @@ urlpatterns = [
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('comments/<int:pk>/update/',
-         CommentUpdateView.as_view(), name='comments-update'),
-    path('comments/<int:pk>/delete/',
-         CommentDeleteView.as_view(), name='comments-delete'),
+    path('comment/<int:pk>/update/',
+         CommentUpdateView.as_view(), name='comment-update'),
+    path('comment/<int:pk>/delete/',
+         CommentDeleteView.as_view(), name='comment-delete'),
 ]
