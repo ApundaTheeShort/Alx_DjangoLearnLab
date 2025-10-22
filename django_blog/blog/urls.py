653959b8c03c2bb54e6566262f14b6500446e00a
Apunda <apundawyclife333@gmail.com>
2025-10-22 10:40:51 +0300

    fix: update comment URL paths for consistency and clarity

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -3,6 +3,8 @@ from . import views
 from .views import PostCreateView, PostUpdateView, PostDetailView, CommentCreateView, CommentUpdateView, CommentDeleteView
 
 urlpatterns = [
+    path('post/<int:pk>/comments/new/',
+         CommentCreateView.as_view(), name='comments-create'),
     path('', views.home, name='home'),
     path('posts/', views.posts, name='posts'),
     path('register/', views.register, name='register'),
@@ -10,7 +12,8 @@ urlpatterns = [
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('post/<int:pk>/comment/new/', CommentCreateView.as_view(), name='comment-create'),
-    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
-    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
+    path('comments/<int:pk>/update/',
+         CommentUpdateView.as_view(), name='comments-update'),
+    path('comments/<int:pk>/delete/',
+         CommentDeleteView.as_view(), name='comments-delete'),
 ]
