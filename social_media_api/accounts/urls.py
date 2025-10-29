992adc33ef816d850af055ca6e036faceb1c7221
Apunda <apundawyclife333@gmail.com>
2025-10-29 07:09:16 +0300

    refactored url patterns for folloe/unfollow

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='user-registration'),
     path('login/', UserLoginView.as_view(), name='user-login'),
     path('profile/', UserProfileView.as_view(), name='user-profile'),
-    path('users/<int:pk>/follow/', FollowUserView.as_view(), name='user-follow'),
-    path('users/<int:pk>/unfollow/', UnfollowUserView.as_view(), name='user-unfollow'),
+    path('follow/<int:user_id>/', FollowUserView.as_view(), name='user-follow'),
+    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='user-unfollow'),
 ]
